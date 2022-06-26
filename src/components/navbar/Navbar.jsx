0d04254816bbf466cd1a38cf4d1f1acc63ce3b6d
horsemandotpy <chonmegai@outlook.com>
2022-06-26 11:35:25 +0700

    Add linking to logo home pages

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import "./navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div>
       <div className="navbar">
         <div className="navContainer">
-          <span className="logo">Flighting.com</span>
+          <Link to="/">
+            <span className="logo">Flighting.com</span>
+          </Link>
           <div className="navItem">
             <button className="navButton">Register</button>
             <button className="navButton">Login</button>
